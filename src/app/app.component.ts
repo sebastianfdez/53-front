b132a6cb8be1e9142885b9ff380b5730d3bf4484
Sebastian Fernandez <sebastian.fernandez@hardis.fr>
2019-09-04 16:37:42 +0200

    description overview component created, using angular resolver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {}
+  constructor(
+    router: Router,
+  ) {
+    // router.events.subscribe(e => console.log(e));
+  }
 }
