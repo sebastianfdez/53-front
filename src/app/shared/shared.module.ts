56aa4730ba3fe1f3f298a33340c41fce28ec06a4
Sebastian <sebastian.fernandez@hardis.fr>
2019-11-15 16:37:40 +0100

    Css fix and scroll bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,7 +11,7 @@ import { GridModule, ExcelModule } from '@progress/kendo-angular-grid';
 import { UploadModule } from '@progress/kendo-angular-upload';
 import { FormsModule } from '@angular/forms';
 import { WarningComponent } from './warning/warning.component';
-import { firebaseKeys } from 'src/firbase-keys';
+import { firebaseKeys } from 'src/firebase-keys';
 import { WarningService } from './warning/warning.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
