b4f0d299ec39c523d77f2439150ca2b7f1885e00
Sebastian Fernandez <sebastian.fernandez@hardis.fr>
2020-04-16 19:19:10 +0200

    Feature: add animations to homepage

--- src/app/public/news/news.component.ts
+++ src/app/public/news/news.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-undef */
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +7,22 @@ import { Component } from '@angular/core';
     templateUrl: './news.component.html',
     styleUrls: ['./news.component.scss'],
 })
-export class NewsComponent {}
+export class NewsComponent {
+    elementInViewport(el: HTMLElement): boolean {
+        const top = el.offsetTop;
+        const height = el.offsetHeight;
+        let screenInit = 0;
+        let screenEnd = 0;
+        let element: HTMLElement = el;
+        while (element.parentElement && screenInit === 0) {
+            if (element.id === 'scrollable') {
+                screenInit = element.scrollTop;
+                screenEnd = screenInit + element.offsetHeight;
+            }
+            element = element.parentElement;
+        }
+        const appear = (top < screenEnd && screenInit < top)
+            || (top + height > screenInit && top + height < screenEnd);
+        return appear;
+    }
+}
